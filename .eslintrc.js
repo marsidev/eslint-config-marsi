c89cac653d3cdc7641521b3f57cefe049ca83a8d
Luis Marsiglia <marsiglia.business@gmail.com>
2022-06-29 11:41:02 -0400

    feat: update rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,116 +1,120 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:import/recommended',
-    'standard'
-  ],
-  plugins: ['react', 'import'],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true
-    }
-  },
-  rules: {
-    'arrow-body-style': 'off',
-    camelcase: 'off',
-    'capitalized-comments': 'off',
-    'comma-dangle': ['warn', 'never'],
-    curly: ['error', 'multi-line'],
-    'guard-for-in': 'off',
-    indent: ['error', 2],
-    'no-mixed-spaces-and-tabs': 'off',
-    'no-console': 'off',
-    'no-debugger': 'warn',
-    'no-negated-condition': 'off',
-    'no-extend-native': 'off',
-    'no-return-assign': 'off',
-    'no-trailing-spaces': 'warn',
-    'no-unused-vars': 'warn',
-    'no-use-before-define': 'off',
-    'object-curly-newline': ['error', { multiline: true, consistent: true }],
-    'object-curly-spacing': ['error', 'always'],
-    'operator-linebreak': 'off',
-    'padding-line-between-statements': 'off',
-    quotes: ['error', 'single'],
-    radix: 'off',
-    semi: ['error', 'never'],
-    'space-before-function-paren': 'off',
-    'spaced-comment': 'warn',
-    'jsx-quotes': ['error', 'prefer-single'],
-    'react/function-component-definition': 0,
-    'react/jsx-indent': [2, 2],
-    'react/jsx-sort-props': [
-      'warn',
-      {
-        callbacksLast: true,
-        shorthandFirst: true,
-        noSortAlphabetically: false,
-        reservedFirst: true
-      }
-    ],
-    'react/jsx-tag-spacing': 'off',
-    'react/jsx-uses-react': 'error',
-    'react/jsx-uses-vars': 'error',
-    'react/no-children-prop': 'off',
-    'react/prop-types': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react/sort-prop-types': [
-      'warn',
-      {
-        callbacksLast: true,
-        ignoreCase: true,
-        requiredFirst: false,
-        sortShapeProp: true,
-        noSortAlphabetically: false
-      }
-    ],
-    'import/order': [
-      'warn',
-      {
-        groups: [
-          'type',
-          'builtin',
-          'object',
-          'external',
-          'internal',
-          'parent',
-          'sibling',
-          'index'
-        ],
-        pathGroups: [
-          {
-            pattern: '~/**',
-            group: 'external',
-            position: 'after'
-          }
-        ],
-        'newlines-between': 'never'
-      }
-    ],
-    'react/self-closing-comp': 'warn'
-  },
-  settings: {
-    react: {
-      version: 'detect'
-    }
-  },
-  ignorePatterns: [
-    'node_modules',
-    'dist',
-    'build',
-    'public',
-    '.next',
-    '.vercel',
-    '.caache',
-    '.eslintrc*',
-    '*.config.js'
-  ]
+	env: {
+		browser: true,
+		es2021: true,
+		node: true
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:import/recommended',
+		'standard'
+	],
+	plugins: ['react', 'import'],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		ecmaFeatures: {
+			jsx: true
+		}
+	},
+	rules: {
+		'arrow-body-style': 'off',
+		camelcase: 'off',
+		'capitalized-comments': 'off',
+		'comma-dangle': ['warn', 'never'],
+		curly: ['error', 'multi-line'],
+		'guard-for-in': 'off',
+		indent: ['error', 2],
+		'no-mixed-spaces-and-tabs': 'off',
+		'no-console': 'off',
+		'no-debugger': 'warn',
+		'no-negated-condition': 'off',
+		'no-extend-native': 'off',
+		'no-return-assign': 'off',
+		'no-trailing-spaces': 'warn',
+		'no-unused-vars': 'warn',
+		'no-use-before-define': 'off',
+		'object-curly-newline': ['error', { multiline: true, consistent: true }],
+		'object-curly-spacing': ['error', 'always'],
+		'operator-linebreak': 'off',
+		'padding-line-between-statements': 'off',
+		quotes: ['error', 'single'],
+		radix: 'off',
+		semi: ['error', 'never'],
+		'space-before-function-paren': 'off',
+		'spaced-comment': 'warn',
+		'jsx-quotes': ['error', 'prefer-single'],
+		'react/function-component-definition': 0,
+		'react/jsx-sort-props': [
+			'warn',
+			{
+				callbacksLast: true,
+				shorthandFirst: true,
+				noSortAlphabetically: false,
+				reservedFirst: true
+			}
+		],
+		'react/jsx-tag-spacing': 'off',
+		'react/jsx-uses-react': 'error',
+		'react/jsx-uses-vars': 'error',
+		'react/no-children-prop': 'off',
+		'react/prop-types': 'off',
+		'react/react-in-jsx-scope': 'off',
+		'react/sort-prop-types': [
+			'warn',
+			{
+				callbacksLast: true,
+				ignoreCase: true,
+				requiredFirst: false,
+				sortShapeProp: true,
+				noSortAlphabetically: false
+			}
+		],
+		'import/order': [
+			'warn',
+			{
+				groups: [
+					'type',
+					'builtin',
+					'object',
+					'external',
+					'internal',
+					'parent',
+					'sibling',
+					'index'
+				],
+				pathGroups: [
+					{
+						pattern: '~/**',
+						group: 'external',
+						position: 'after'
+					}
+				],
+				'newlines-between': 'never'
+			}
+		],
+		'react/self-closing-comp': 'warn',
+		'sort-imports': ['warn', { ignoreDeclarationSort: true }],
+		'no-constant-binary-expression': 'warn',
+		indent: ['error', 'tab'],
+		'no-tabs': 'off',
+		'react/jsx-indent': ['error', 'tab']
+	},
+	settings: {
+		react: {
+			version: 'detect'
+		}
+	},
+	ignorePatterns: [
+		'node_modules',
+		'dist',
+		'build',
+		'public',
+		'.next',
+		'.vercel',
+		'.caache',
+		'.eslintrc*',
+		'*.config.js'
+	]
 }
--- typescript.js
+++ typescript.js
@@ -1,88 +1,88 @@
 module.exports = {
-  extends: [
-    './.eslintrc.js',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:import/typescript'
-  ],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  parserOptions: {
-    project: './tsconfig.json'
-  },
-  rules: {
-    '@typescript-eslint/no-unused-vars': [
-      'warn',
-      {
-        args: 'after-used',
-        ignoreRestSiblings: false,
-        argsIgnorePattern: '^_.*?$'
-      }
-    ],
-    '@typescript-eslint/semi': ['error', 'never'],
-    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
-    '@typescript-eslint/consistent-type-assertions': [
-      'error',
-      {
-        assertionStyle: 'as',
-        objectLiteralTypeAssertions: 'never'
-      }
-    ],
-    '@typescript-eslint/member-delimiter-style': [
-      'error',
-      {
-        multiline: {
-          delimiter: 'none',
-          requireLast: true
-        },
-        singleline: {
-          delimiter: 'comma',
-          requireLast: false
-        },
-        multilineDetection: 'brackets'
-      }
-    ],
-    '@typescript-eslint/method-signature-style': ['error', 'property'],
-    '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/no-floating-promises': 'off',
-    '@typescript-eslint/no-redeclare': [
-      'warn',
-      {
-        ignoreDeclarationMerge: true
-      }
-    ],
-    '@typescript-eslint/type-annotation-spacing': ['error'],
-    'import/no-unresolved': 'error'
-  },
-  settings: {
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx']
-    },
-    'import/resolver': {
-      typescript: {
-        alwaysTryTypes: true // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+	extends: [
+		'./.eslintrc.js',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:import/typescript'
+	],
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint'],
+	parserOptions: {
+		project: './tsconfig.json'
+	},
+	rules: {
+		'@typescript-eslint/no-unused-vars': [
+			'warn',
+			{
+				args: 'after-used',
+				ignoreRestSiblings: false,
+				argsIgnorePattern: '^_.*?$'
+			}
+		],
+		'@typescript-eslint/semi': ['error', 'never'],
+		'@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+		'@typescript-eslint/consistent-type-assertions': [
+			'error',
+			{
+				assertionStyle: 'as',
+				objectLiteralTypeAssertions: 'never'
+			}
+		],
+		'@typescript-eslint/member-delimiter-style': [
+			'error',
+			{
+				multiline: {
+					delimiter: 'none',
+					requireLast: true
+				},
+				singleline: {
+					delimiter: 'comma',
+					requireLast: false
+				},
+				multilineDetection: 'brackets'
+			}
+		],
+		'@typescript-eslint/method-signature-style': ['error', 'property'],
+		'@typescript-eslint/no-explicit-any': 'warn',
+		'@typescript-eslint/no-floating-promises': 'off',
+		'@typescript-eslint/no-redeclare': [
+			'warn',
+			{
+				ignoreDeclarationMerge: true
+			}
+		],
+		'@typescript-eslint/type-annotation-spacing': ['error'],
+		'import/no-unresolved': 'error'
+	},
+	settings: {
+		'import/parsers': {
+			'@typescript-eslint/parser': ['.ts', '.tsx']
+		},
+		'import/resolver': {
+			typescript: {
+				alwaysTryTypes: true // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
 
-        // Choose from one of the "project" configs below or omit to use <root>/tsconfig.json by default
+				// Choose from one of the "project" configs below or omit to use <root>/tsconfig.json by default
 
-        // // use <root>/path/to/folder/tsconfig.json
-        // "project": "path/to/folder",
+				// // use <root>/path/to/folder/tsconfig.json
+				// "project": "path/to/folder",
 
-        // // Multiple tsconfigs (Useful for monorepos)
+				// // Multiple tsconfigs (Useful for monorepos)
 
-        // // use a glob pattern
-        // "project": "packages/*/tsconfig.json",
+				// // use a glob pattern
+				// "project": "packages/*/tsconfig.json",
 
-        // // use an array
-        // "project": [
-        //   "packages/module-a/tsconfig.json",
-        //   "packages/module-b/tsconfig.json"
-        // ],
+				// // use an array
+				// "project": [
+				//   "packages/module-a/tsconfig.json",
+				//   "packages/module-b/tsconfig.json"
+				// ],
 
-        // // use an array of glob patterns
-        // "project": [
-        //   "packages/*/tsconfig.json",
-        //   "other-packages/*/tsconfig.json"
-        // ]
-      }
-    }
-  }
+				// // use an array of glob patterns
+				// "project": [
+				//   "packages/*/tsconfig.json",
+				//   "other-packages/*/tsconfig.json"
+				// ]
+			}
+		}
+	}
 }
